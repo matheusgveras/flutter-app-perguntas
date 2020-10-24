cc6e2edf652e755075242ca6b895d310795ce49c
Matheus Veras <matheus.veras@lobofrontal.com.br>
2020-10-24 08:09:51 -0300

    Lista para widgets

--- lib/main.dart
+++ lib/main.dart
@@ -13,24 +13,32 @@ class _PerguntaAppState extends State<PerguntaApp> {
     setState(() {
       _perguntaSelecionada++;
     });
-    print('pergunta respondida'); 
+    
   }
   @override
   Widget build(BuildContext context) {
 
-    final perguntas = [
-      'Qual meu nome?',
-      'Qual meu signo?',
-      'Qual meu idade?',
-      'Qual meu computador?',
-      'Qual minha cor?',
-      'Qual meu chienelo?',
-      'Qual meu animal?',
-      'Qual meu mascote?',
-      'Qual meu time?',
-      'Qual meu teste?',
+    final List<Map<String, Object>> perguntas = [
+     {
+       'texto': 'Pergunta #1',
+       'resposta': ['#1 Resp1','#1 Resp2','#1 Resp3','#1 Resp4']
+     },
+     {
+       'texto': 'Pergunta #2',
+       'resposta': ['#2 Resp1','#2 Resp2','#1 Resp2','#2 Resp4']
+     },
+     {
+       'texto': 'Pergunta #3',
+       'resposta': ['#3 Resp1','#3 Resp2','#3 Resp3','#3 Resp4']
+     },
+     {
+       'texto': 'Pergunta #4',
+       'resposta': ['#4 Resp1','#4 Resp2','#4 Resp3','#4 Resp4']
+     }
     ];
-
+    for(textResp in perguntas[_perguntaSelecionada]['resposta']){
+      
+    }
     return MaterialApp(
       title: 'Ola Mundo 2',
       home: Scaffold(
@@ -39,10 +47,10 @@ class _PerguntaAppState extends State<PerguntaApp> {
         ),
         body: Column(
           children: [
-            Questao(perguntas[_perguntaSelecionada]),
-            Resposta('Resposta 1'),
-            Resposta('Resposta 2'),
-            Resposta('Resposta 3'),
+            Questao(perguntas[_perguntaSelecionada]['texto']),
+            Resposta('Resposta 1', _responder),
+            Resposta('Resposta 2', _responder),
+            Resposta('Resposta 3', _responder)
           ],
         )
       )
--- lib/resposta.dart
+++ lib/resposta.dart
@@ -2,9 +2,18 @@ import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
   final String textoBotao;
-  Resposta(this.textoBotao);
+  final void Function() quandoSelecionado;
+  Resposta(this.textoBotao, this.quandoSelecionado);
   @override
   Widget build(BuildContext context) {
-    return  RaisedButton(child: Text(textoBotao), onPressed: null);
+    return  Container(
+      width: double.infinity,
+      child: RaisedButton(
+        textColor: Colors.white,
+        color: Colors.red,
+        child:Text(textoBotao), 
+        onPressed: quandoSelecionado
+        ),
+    );
   }
 }
